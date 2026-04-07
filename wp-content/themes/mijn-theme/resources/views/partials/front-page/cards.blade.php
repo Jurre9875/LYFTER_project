88c7123c58300bf8acab9b2cdbaa7a98e2416204
jurre9875 <101713@glr.nl>
2026-04-07 12:52:04 -0700

    Nieuwe cards sectie toegevoegd

---
+++ wp-content/themes/mijn-theme/resources/views/partials/front-page/cards.blade.php
@@ -0,0 +1,38 @@
+<section class="bg-white px-8 py-24">
+  <div class="mx-auto max-w-[1440px]">
+    <div class="cards-grid mx-auto grid max-w-[1240px] grid-cols-3 gap-8">
+      <div class="card">
+        <div class="card-head">
+          <img alt="" class="card-icon" src="{{ Vite::asset('resources/images/icons/iconschield.png') }}" />
+          <img alt="" class="card-arrow" src="{{ Vite::asset('resources/images/icons/iconarrow.png') }}" />
+        </div>
+        <h3 class="card-title">Effortless Security</h3>
+        <p class="card-text">
+          Unlock peace of mind with our garage door opener, effortless convenience coupled with robust security at the touch of a button.
+        </p>
+      </div>
+
+      <div class="card">
+        <div class="card-head">
+          <img alt="" class="card-icon" src="{{ Vite::asset('resources/images/icons/iconhuisje.png') }}" />
+          <img alt="" class="card-arrow" src="{{ Vite::asset('resources/images/icons/iconarrow.png') }}" />
+        </div>
+        <h3 class="card-title">Seamless Integration</h3>
+        <p class="card-text">
+          Elevate your life with our garage door opener, seamless integration into your smart home for a connected living experience.
+        </p>
+      </div>
+
+      <div class="card">
+        <div class="card-head">
+          <img alt="" class="card-icon" src="{{ Vite::asset('resources/images/icons/icongarage.png') }}" />
+          <img alt="" class="card-arrow" src="{{ Vite::asset('resources/images/icons/iconarrow.png') }}" />
+        </div>
+        <h3 class="card-title">Reliability Redefined</h3>
+        <p class="card-text">
+          Trust in consistent performance, our garage door opener is engineered for enduring reliability, simplifying your daily routine.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
